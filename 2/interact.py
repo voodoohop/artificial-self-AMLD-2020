655dbc032e82e580850f9911d429a851db24fdb1
Riccardo Orlando <orlandoricc@gmail.com>
2020-03-14 16:21:09 +0100

    Added Bert-like models for multilingual dataset

--- 2/interact.py
+++ 2/interact.py
@@ -7,14 +7,22 @@ from pprint import pformat
 import warnings
 import torch
 import torch.nn.functional as F
-from transformers import OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import (
+    OpenAIGPTLMHeadModel,
+    OpenAIGPTTokenizer,
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+)
 from train import set_seed
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s",
+)
 
 
-def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')):
+def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float("Inf")):
     """ Filter a distribution of logits using top-k and/or nucleus (top-p) filtering
         Args:
             logits: logits distribution shape (batch size x vocabulary size)
@@ -40,9 +48,12 @@ def top_k_top_p_filtering(logits, top_k=0, top_p=0.0, filter_value=-float('Inf')
         sorted_indices_to_remove[..., 0] = 0
 
         # scatter sorted tensors to original indexing
-        indices_to_remove = sorted_indices_to_remove.scatter(dim=1, index=sorted_indices, src=sorted_indices_to_remove)
+        indices_to_remove = sorted_indices_to_remove.scatter(
+            dim=1, index=sorted_indices, src=sorted_indices_to_remove
+        )
         logits[indices_to_remove] = filter_value
     return logits
+
 
 def sample_sequence(conversation, model, args, num_samples=1):
     context = torch.tensor(conversation, dtype=torch.long, device=args.device)
@@ -50,48 +61,105 @@ def sample_sequence(conversation, model, args, num_samples=1):
     generated = context
     with torch.no_grad():
         for _ in range(args.max_length):
-            inputs = {'input_ids': generated}
+            inputs = {"input_ids": generated}
             outputs = model(**inputs)
             # scale by temperature
-            next_token_logits = outputs[0][:, -1, :] / (args.temperature if args.temperature > 0 else 1.)
+            next_token_logits = outputs[0][:, -1, :] / (
+                args.temperature if args.temperature > 0 else 1.0
+            )
             # filter by top-k/top-p
-            filtered_logits = top_k_top_p_filtering(next_token_logits, top_k=args.top_k, top_p=args.top_p)
-            if args.temperature == 0: # greedy sampling:
+            filtered_logits = top_k_top_p_filtering(
+                next_token_logits, top_k=args.top_k, top_p=args.top_p
+            )
+            if args.temperature == 0:  # greedy sampling:
                 next_token = torch.argmax(filtered_logits, dim=-1).unsqueeze(-1)
             else:
-                next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+                next_token = torch.multinomial(
+                    F.softmax(filtered_logits, dim=-1), num_samples=1
+                )
             generated = torch.cat((generated, next_token), dim=1)
     return generated
 
 
 def run():
     parser = ArgumentParser()
-    parser.add_argument("--run_name", type=str, default='run1', help="The name of the run (subdirectory in ./runs)")
-    parser.add_argument("--model", type=str, default="openai-gpt", help="Model type (openai-gpt or gpt2)", choices=['openai-gpt', 'gpt2'])
-    parser.add_argument("--max_history", type=int, default=2, help="Number of previous utterances to keep in history")
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
-    parser.add_argument("--no_sample", action='store_true', help="Set to use greedy decoding instead of sampling")
-    parser.add_argument("--max_length", type=int, default=80, help="Maximum length of the output utterances")
+    parser.add_argument(
+        "--run_name",
+        type=str,
+        default="run1",
+        help="The name of the run (subdirectory in ./runs)",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="openai-gpt",
+        help="Model type (openai-gpt or gpt2)",
+        choices=["openai-gpt", "gpt2"],
+    )
+    parser.add_argument(
+        "--max_history",
+        type=int,
+        default=2,
+        help="Number of previous utterances to keep in history",
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device (cuda or cpu)",
+    )
+    parser.add_argument(
+        "--no_sample",
+        action="store_true",
+        help="Set to use greedy decoding instead of sampling",
+    )
+    parser.add_argument(
+        "--max_length",
+        type=int,
+        default=80,
+        help="Maximum length of the output utterances",
+    )
     parser.add_argument("--seed", type=int, default=42, help="Seed")
-    parser.add_argument("--temperature", type=int, default=1.0, help="Sampling softmax temperature")
-    parser.add_argument("--top_k", type=int, default=0, help="Filter top-k tokens before sampling (<=0: no filtering)")
-    parser.add_argument("--top_p", type=float, default=0.8, help="Nucleus filtering (top-p) before sampling (<=0.0: no filtering)")
-    parser.add_argument("--no_info", action='store_true', default=False, help="Only show conversation output")
+    parser.add_argument(
+        "--temperature", type=int, default=1.0, help="Sampling softmax temperature"
+    )
+    parser.add_argument(
+        "--top_k",
+        type=int,
+        default=0,
+        help="Filter top-k tokens before sampling (<=0: no filtering)",
+    )
+    parser.add_argument(
+        "--top_p",
+        type=float,
+        default=0.8,
+        help="Nucleus filtering (top-p) before sampling (<=0.0: no filtering)",
+    )
+    parser.add_argument(
+        "--no_info",
+        action="store_true",
+        default=False,
+        help="Only show conversation output",
+    )
     args = parser.parse_args()
 
     # set seed
     set_seed(args)
 
     logger.info("Get pretrained model and tokenizer")
-    model_path = os.path.join('runs', args.run_name)
-    tokenizer_class, model_class = (GPT2Tokenizer, GPT2LMHeadModel) if args.model == 'gpt2' else (OpenAIGPTTokenizer, OpenAIGPTLMHeadModel)
+    model_path = os.path.join("runs", args.run_name)
+    tokenizer_class, model_class = (
+        (GPT2Tokenizer, GPT2LMHeadModel)
+        if args.model == "gpt2"
+        else (OpenAIGPTTokenizer, OpenAIGPTLMHeadModel)
+    )
     tokenizer = tokenizer_class.from_pretrained(model_path)
     model = model_class.from_pretrained(model_path)
     model.to(args.device)
     history = []
     personality = []
-    speaker1_tag = '<speaker1>'
-    speaker2_tag = '<speaker2>'
+    speaker1_tag = "<speaker2>"
+    speaker2_tag = "<speaker1>"
     speaker1_tag_id = tokenizer.convert_tokens_to_ids(speaker1_tag)
     speaker2_tag_id = tokenizer.convert_tokens_to_ids(speaker2_tag)
     history = f"""
@@ -103,41 +171,42 @@ def run():
 {speaker1_tag} Sure, what do you want to talk about?"""
     print(history)
     print('\n[Chat with the model! Send "h" to see the full history]\n')
-    history = history.split('\n')
-    while True: 
+    history = history.split("\n")
+    while True:
         message = None
         while not message:
-            message = input(f'{speaker2_tag} ')
-            if message == 'h':
-                print('\n'.join(history))
+            message = input(f"{speaker2_tag} ")
+            if message == "h":
+                print("\n".join(history))
                 message = None
         # add new message to history
-        history.append(f'{speaker2_tag} {message}')
+        history.append(f"{speaker2_tag} {message}")
         # keep only most recent conversation as input to the model
-        recent_history = history[-(2*args.max_history):]
+        recent_history = history[-(2 * args.max_history) :]
         # concatenate history into single string and add trigger word "bot:"
-        history_str = '{}\n{}'.format('\n'.join(recent_history), speaker1_tag)
+        history_str = "{}\n{}".format("\n".join(recent_history), speaker1_tag)
         # tokenize text and convert into vocabulary ids (input ids)
         history_enc = tokenizer.encode(history_str, add_special_tokens=True)
         with torch.no_grad():
             out_ids = sample_sequence(history_enc, model, args)
-        out_ids = out_ids[:, len(history_enc):].tolist()[0]
+        out_ids = out_ids[:, len(history_enc) :].tolist()[0]
         if not args.no_info:
-            print(20*'-')
-            print('Output of model:')
+            print(20 * "-")
+            print("Output of model:")
             full_output = tokenizer.decode(out_ids, clean_up_tokenization_spaces=True)
             print(full_output)
-            print('\nInput to the model:')
+            print("\nInput to the model:")
             print(history_str)
-            print(20*'-' + '\n')
+            print(20 * "-" + "\n")
         # Select part before speaker tags as answer
         for i, out_id in enumerate(out_ids):
             if out_id in [speaker1_tag_id, speaker2_tag_id]:
                 break
-        answer = '{} {}'.format(speaker1_tag, tokenizer.decode(out_ids[:i]))
+        answer = "{} {}".format(speaker1_tag, tokenizer.decode(out_ids[:i]))
         print(answer)
         # add answer to history
         history.append(answer)
+
 
 if __name__ == "__main__":
     run()
--- 2/train.py
+++ 2/train.py
@@ -21,13 +21,21 @@ from transformers import (
     get_linear_schedule_with_warmup,
 )
 from utils import get_input_task2, set_seed, add_special_tokens_
+import transformers as tr
+
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)-5.5s] [%(name)-12.12s]: %(message)s",
+)
+
 
 class TextDataset(Dataset):
     def __init__(self, tokenizer, args):
-        text = get_input_task2(args.data_path)
+        text = get_input_task2(
+            args.data_path, speaker1_tag="<speaker2>", speaker2_tag="<speaker1>"
+        )
         logger.info("Tokenizing and building input...")
         tokenized_text = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(text))
         self.examples = []
@@ -35,13 +43,22 @@ class TextDataset(Dataset):
         if block_size < 0:
             # by default use maximum possible input block size
             block_size = tokenizer.max_len_single_sentence
-        for i in range(0, len(tokenized_text) - block_size + 1, block_size):  # Truncate in block of block_size
+        for i in range(
+            0, len(tokenized_text) - block_size + 1, block_size
+        ):  # Truncate in block of block_size
             # Note that we are loosing the last truncated example here for the sake of simplicity (no padding)
-            self.examples.append(tokenizer.build_inputs_with_special_tokens(tokenized_text[i : i + block_size]))
+            self.examples.append(
+                tokenizer.build_inputs_with_special_tokens(
+                    tokenized_text[i : i + block_size]
+                )
+            )
+
     def __len__(self):
         return len(self.examples)
+
     def __getitem__(self, item):
         return torch.tensor(self.examples[item])
+
 
 def get_data_loader(args, tokenizer):
     """ Prepare the dataset for training and evaluation """
@@ -51,24 +68,76 @@ def get_data_loader(args, tokenizer):
     data_loader = DataLoader(dataset, batch_size=args.train_batch_size, shuffle=True)
     return data_loader
 
+
 def train():
     parser = ArgumentParser()
-    parser.add_argument("--data_path", default=None, help="Path to conversational data (by default will look for single file in ./data)")
-    parser.add_argument("--run_name", type=str, default='run1', help="The name of the run (subdirectory in ./runs)")
-    parser.add_argument("--model", type=str, default="openai-gpt", choices=['openai-gpt', 'gpt2'], help="Initialize model from path to checkpoint or with model name (openai-gpt/openai-gpt2)")
-    parser.add_argument("--save_every", type=int, default=50, help="Save checkpoint every n updates steps.")
-    parser.add_argument("--max_input_length", type=int, default=400, help="Number of tokens which will be fed into the model (reduce this number if you have memory constraints)")
-    parser.add_argument("--weight_decay", default=0.0, type=float, help="Weight decay if we apply some.")
-    parser.add_argument("--train_batch_size", type=int, default=4, help="Batch size for training")
-    parser.add_argument("--valid_batch_size", type=int, default=4, help="Batch size for validation")
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=8, help="Accumulate gradients on several steps")
+    parser.add_argument(
+        "--data_path",
+        default=None,
+        help="Path to conversational data (by default will look for single file in ./data)",
+    )
+    parser.add_argument(
+        "--run_name",
+        type=str,
+        default="run1",
+        help="The name of the run (subdirectory in ./runs)",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="openai-gpt",
+        # choices=["openai-gpt", "gpt2"],
+        help="Initialize model from path to checkpoint or with model name (openai-gpt/openai-gpt2)",
+    )
+    parser.add_argument(
+        "--save_every",
+        type=int,
+        default=50,
+        help="Save checkpoint every n updates steps.",
+    )
+    parser.add_argument(
+        "--max_input_length",
+        type=int,
+        default=400,
+        help="Number of tokens which will be fed into the model (reduce this number if you have memory constraints)",
+    )
+    parser.add_argument(
+        "--weight_decay", default=0.0, type=float, help="Weight decay if we apply some."
+    )
+    parser.add_argument(
+        "--train_batch_size", type=int, default=4, help="Batch size for training"
+    )
+    parser.add_argument(
+        "--valid_batch_size", type=int, default=4, help="Batch size for validation"
+    )
+    parser.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=8,
+        help="Accumulate gradients on several steps",
+    )
     parser.add_argument("--lr", type=float, default=5e-5, help="Learning rate")
-    parser.add_argument("--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer.")
-    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipping gradient norm")
-    parser.add_argument("--n_epochs", type=int, default=2, help="Number of training epochs")
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu", help="Device (cuda or cpu)")
-    parser.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
-    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+    parser.add_argument(
+        "--adam_epsilon", default=1e-8, type=float, help="Epsilon for Adam optimizer."
+    )
+    parser.add_argument(
+        "--max_norm", type=float, default=1.0, help="Clipping gradient norm"
+    )
+    parser.add_argument(
+        "--n_epochs", type=int, default=2, help="Number of training epochs"
+    )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda" if torch.cuda.is_available() else "cpu",
+        help="Device (cuda or cpu)",
+    )
+    parser.add_argument(
+        "--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps."
+    )
+    parser.add_argument(
+        "--seed", type=int, default=42, help="random seed for initialization"
+    )
     args = parser.parse_args()
 
     # Set seed
@@ -76,11 +145,13 @@ def train():
 
     # Load tokenizer
     logger.info("Prepare tokenizer, pretrained model and optimizer.")
-    tokenizer_class = GPT2Tokenizer if "gpt2" in args.model else OpenAIGPTTokenizer # cant use Autotokenizer because checkpoint could be a Path
-    tokenizer = tokenizer_class.from_pretrained(args.model)
+    # tokenizer_class = (
+    #     GPT2Tokenizer if "gpt2" in args.model else OpenAIGPTTokenizer
+    # )  # cant use Autotokenizer because checkpoint could be a Path
+    tokenizer = tr.AutoTokenizer.from_pretrained(args.model)
     # Load model
-    model_class = GPT2LMHeadModel if "gpt2" in args.model else OpenAIGPTLMHeadModel
-    model = model_class.from_pretrained(args.model)
+    # model_class = GPT2LMHeadModel if "gpt2" in args.model else OpenAIGPTLMHeadModel
+    model = tr.AutoModelWithLMHead.from_pretrained(args.model)
     model.to(args.device)
     # Add special tokens if they are not already added
     add_special_tokens_(model, tokenizer)
@@ -93,10 +164,21 @@ def train():
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
         {
-            "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
             "weight_decay": args.weight_decay,
         },
-        {"params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], "weight_decay": 0.0},
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
     ]
     optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, eps=args.adam_epsilon)
     t_total = len(data_loader) // args.gradient_accumulation_steps * args.n_epochs
@@ -114,12 +196,20 @@ def train():
     if os.path.exists(args.model):
         # set global_step to gobal_step of last saved checkpoint from model path
         global_step = int(args.model.split("-")[-1].split("/")[0])
-        epochs_trained = global_step // (len(data_loader) // args.gradient_accumulation_steps)
-        steps_trained_in_current_epoch = global_step % (len(data_loader) // args.gradient_accumulation_steps)
-        logger.info("Continuing training from checkpoint, will skip to saved global_step")
+        epochs_trained = global_step // (
+            len(data_loader) // args.gradient_accumulation_steps
+        )
+        steps_trained_in_current_epoch = global_step % (
+            len(data_loader) // args.gradient_accumulation_steps
+        )
+        logger.info(
+            "Continuing training from checkpoint, will skip to saved global_step"
+        )
         logger.info(f"Continuing training from epoch {epochs_trained}")
         logger.info(f"Continuing training from global step {global_step}")
-        logger.info(f"Will skip the first {steps_trained_in_current_epoch} steps in the first epoch")
+        logger.info(
+            f"Will skip the first {steps_trained_in_current_epoch} steps in the first epoch"
+        )
 
     # Training loop
     model.zero_grad()
@@ -137,10 +227,10 @@ def train():
             inputs, labels = (batch, batch)
             inputs = inputs.to(args.device)
             labels = labels.to(args.device)
-            loss, *_ = model(inputs, labels=labels)
+            loss, *_ = model(inputs, masked_lm_labels=labels)
             loss.backward()
             tr_loss = loss.item()
-            av_loss = (step*av_loss + tr_loss)/(step + 1)
+            av_loss = (step * av_loss + tr_loss) / (step + 1)
             pbar.set_description(f"Average loss: {av_loss:.4f}")
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_norm)
             if (step + 1) % args.gradient_accumulation_steps == 0:
@@ -150,18 +240,28 @@ def train():
                 global_step += 1
                 if global_step % args.save_every == 0 and global_step > 0:
                     checkpoint_prefix = "checkpoint"
-                    output_dir = os.path.join('runs', args.run_name, "{}-{}".format(checkpoint_prefix, global_step))
+                    output_dir = os.path.join(
+                        "runs",
+                        args.run_name,
+                        "{}-{}".format(checkpoint_prefix, global_step),
+                    )
                     if not os.path.exists(output_dir):
                         os.makedirs(output_dir)
                     logger.info(f"Saving model checkpoint to {output_dir}")
                     model.save_pretrained(output_dir)
                     tokenizer.save_pretrained(output_dir)
-                    logger.info(f"Saving optimizer and scheduler states to {output_dir}")
-                    torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
-                    torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
+                    logger.info(
+                        f"Saving optimizer and scheduler states to {output_dir}"
+                    )
+                    torch.save(
+                        optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt")
+                    )
+                    torch.save(
+                        scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt")
+                    )
 
     # save model
-    output_dir = os.path.join('runs', args.run_name)
+    output_dir = os.path.join("runs", args.run_name)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     logger.info(f"Saving model checkpoint to {output_dir}")
@@ -169,6 +269,7 @@ def train():
     tokenizer.save_pretrained(output_dir)
     # Good practice: save your training arguments together with the trained model
     torch.save(args, os.path.join(output_dir, "training_args.bin"))
+
 
 if __name__ == "__main__":
     train()
